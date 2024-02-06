47c62fb9278082f652b495e64f42078610714818
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-06 09:32:03 -0700

    started project, removed view in router config

---
+++ app/router-config.js
@@ -0,0 +1,16 @@
+import { ExamplesController } from "./controllers/ExamplesController.js";
+import { HomeController } from "./controllers/HomeController.js";
+import { Router } from "./utils/Router.js";
+
+
+export const router = new Router([
+  {
+    path: '',
+    controllers: [HomeController, ExamplesController],
+    view: ''
+  },
+  {
+    path: '#/about',
+    view: 'app/views/AboutView.html'
+  }
+])

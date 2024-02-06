61852213964988597a2e61f1bff96f7f50c9166d
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-06 11:03:33 -0700

    created Gachamon class

--- app/controllers/CoinsController.js
+++ app/controllers/CoinsController.js
@@ -23,4 +23,3 @@ export class CoinsController {
   }
 
 }
-
---
+++ app/models/Gachamon.js
@@ -0,0 +1,13 @@
+import { AppState } from "../AppState.js"
+
+
+export class Gachamon {
+  constructor (data) {
+    debugger
+    this.name = data.name
+    this.emoji = data.emoji
+    this.rarity = data.rarity
+  }
+}
+
+

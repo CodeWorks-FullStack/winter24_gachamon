c0dad6b2c89beb0d077f6d2156d4f3226f23d585
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-06 11:15:13 -0700

    removed name from appstate

--- app/AppState.js
+++ app/AppState.js
@@ -5,8 +5,6 @@ import { createObservableProxy } from './utils/ObservableProxy.js'
 class ObservableAppState extends EventEmitter {
   coins = 0
 
-  name = ''
-
   gachamons = [
     new Gachamon({ name: 'Doctor Alligator', emoji: '🐊', rarity: 'common' }),
     new Gachamon({ name: 'Larry', emoji: '🦆', rarity: 'uncommon' })

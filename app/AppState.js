8a2076244ffecfdabaf90d6bf64064349f1e3520
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-06 09:51:03 -0700

    removed examples array in appstate

--- app/AppState.js
+++ app/AppState.js
@@ -2,10 +2,6 @@ import { EventEmitter } from './utils/EventEmitter.js'
 import { createObservableProxy } from './utils/ObservableProxy.js'
 
 class ObservableAppState extends EventEmitter {
-
-  /**@type {import('./models/Example.js').Example[]} */
-  examples = []
-
   coins = 0
 }
 

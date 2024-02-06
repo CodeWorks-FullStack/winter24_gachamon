08f6fdc66bae9e96b4393a0b365a6d894bc13c59
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-06 10:07:09 -0700

    making coins go up

--- app/services/CoinsService.js
+++ app/services/CoinsService.js
@@ -1,6 +1,15 @@
+import { AppState } from "../AppState.js";
+
 class CoinsService {
   addCoin() {
     console.log('Adding coin, but in the service');
+
+    // REVIEW makes a new value in memory, because coins is a primitive
+    // let coins = AppState.coins
+
+    // manipulate the value stored in the object directly
+    AppState.coins++
+    console.log('coins in appstate', AppState.coins);
   }
 }
 
